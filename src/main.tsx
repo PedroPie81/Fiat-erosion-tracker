b17436f19e5e1f49b6e9123bf88fc3a80d065204
PedroPie81 <peteradamj@hotmail.co.uk>
2026-02-23 23:38:47 +0000

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+// import './index.css'  // delete this line if you don't have index.css in src/
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
